ff99e9925078d6054b84428c372dfcf43b7b0eb7
akhilamaniyan <80851158+akhilamaniyan@users.noreply.github.com>
2021-03-17 12:20:02 -0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,33 @@
+<!-- jQuery first, then Bootstrap JS. -->
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>-->
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+
+      <script type="text/javascript">
+
+        $(".toggleForms").click(function() {
+
+            $("#signUpForm").toggle();
+            $("#logInForm").toggle();
+
+        });
+
+
+
+/*      $("#diary").on('change keyup paste', function() {
+                $.ajax({
+
+                method: "POST",
+                url: "updatedatabase.php"
+                data:{ content: $("#diary").val()}
+
+                });
+
+});
+*/
+ </script>
+
+  </body>
+</html>
